095d2a3cfd444db63607c143a37047c685b23f00
Soobum Kim <skim743@gatech.edu>
2023-09-24 20:00:26 -0400

    Code clean-ups

--- gritsbot/utils/detect_serial.py
+++
@@ -1,8 +0,0 @@
-import serial.tools.list_ports
-
-
-def main():
-    print([comport.device for comport in serial.tools.list_ports.comports() if 'ttyACM' in comport.device][0])
-
-if __name__ == '__main__':
-    main()
