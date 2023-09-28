8a756eb846f12a3f7631a6a59d68bb7f7c24bfe5
Soobum Kim <skim743@gatech.edu>
2023-09-27 21:37:38 -0400

    Fixed typo in docker_build.sh

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker build --no-cache --build-arg ROBO_HOST=$1 --build-arg ROBO_PORT=$2 --tag GTernal:firmware .
+sudo docker build --no-cache --build-arg ROBO_HOST=$1 --build-arg ROBO_PORT=$2 --tag gternal:firmware .
