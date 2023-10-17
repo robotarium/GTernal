1d50c0680a9a4e8905fe7e7d0016b502ea56506f
Soobum Kim <skim743@gatech.edu>
2023-10-16 21:27:55 -0400

    Testing docker_run.sh with local config files

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -6,4 +6,8 @@ docker run -d \
 	   --name firmware \
 	   --net host \
 	   --device $(sudo -u pi python3 ../firmware/RPi/detect_serial.py):/dev/ttyAMA0 \
-	   skim743/firmware
+	   skim743/firmware \
+	   python3 GTernal/firmware/RPi/pi_firmware.py \
+	   -host '192.168.1.8' \
+	   -port '1884' \
+	   GTernal/config/mac_list.json
