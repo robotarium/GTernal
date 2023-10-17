18fe92bbb11cd6600069fb232e7535aec4e0adb5
Soobum Kim <skim743@gatech.edu>
2023-10-16 21:57:44 -0400

    Updated docker_build.sh

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 (cd ../; sudo docker build --no-cache \
-                           --build-arg ROBO_HOST=$1 \
-                           --build-arg ROBO_PORT=$2 \
                            --tag gternal:firmware \
                            -f docker/Dockerfile .)
